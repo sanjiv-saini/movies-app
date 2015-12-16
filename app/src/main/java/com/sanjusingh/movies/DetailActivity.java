f313173a734b3f24eae071e0f5fbb875418956c6
Sanju Singh <sanju_saini@outlook.com>
2015-12-16 15:26:05 +0530

    Refactors the code

--- app/src/main/java/com/sanjusingh/movies/DetailActivity.java
+++ app/src/main/java/com/sanjusingh/movies/DetailActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -69,9 +68,7 @@ public class DetailActivity extends ActionBarActivity {
 
             Intent intent = getActivity().getIntent();
             if((intent != null) && intent.hasExtra("movie")){
-                Log.v("detailFragment", "Got some extra movies");
                 selectedMovie = intent.getExtras().getParcelable("movie");
-
             }
 
             TextView titleView = (TextView) rootView.findViewById(R.id.titleView);
